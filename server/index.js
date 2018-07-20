a61f900bb197cc65fc869cc31f4699ee79120cca
Calin Lewis <calin.lewis@gmail.com>
2018-07-20 15:24:44 -0700

    Add 'body-parser'; Create basic handling for 'home' and 'redirect' endpoints

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
+const jsonParser = bodyParser.json();
+
+// Will need a request on load to this endpoint in order to serve
+// up static files:
+// app.use('/static', express.static(path.join(__dirname, '/../client/dist')));
+
+app.get('/home', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/redirect', jsonParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  res.send('A redirect action has been recieved');
 });
 
 app.listen(3001);

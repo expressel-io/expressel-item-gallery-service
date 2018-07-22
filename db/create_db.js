5f77bf2eed7931321c20dd47b1057a90a1167799
Calin Lewis <calin.lewis@gmail.com>
2018-07-21 17:10:18 -0700

    Edit config.yml for mySQL

--- db/create_db.js
+++ db/create_db.js
@@ -4,9 +4,9 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-})
+});
 
 connection.query('SHOW databases;', (error, results) => {
   if (error) throw error;
-  console.log(results)
+  console.log(results);
 });

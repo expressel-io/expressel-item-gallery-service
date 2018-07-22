af5959e6880ea50166cac52afc72bfade08a0d3c
Calin Lewis <calin.lewis@gmail.com>
2018-07-21 19:44:09 -0700

    Fix syntax error

--- unit_testing/schema.test.js
+++ unit_testing/schema.test.js
@@ -100,7 +100,7 @@ describe('database population', () => {
   test('First row of the database should be the Macbook Pro', () => {
     connection.query('USE items;', (error) => {
       if (error) throw error;
-      connection.query('SELECT * FROM items WHERE Item_ID = 1', (err, result) => {
+      connection.query('SELECT * FROM items WHERE Item_ID = 1;', (err, result) => {
         if (err) throw error;
         expect(result).toBeNull();
       });

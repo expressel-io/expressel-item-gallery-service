f1368d9ce174362c094c8b1a8796f9498f238c51
Calin Lewis <calin.lewis@gmail.com>
2018-07-25 17:34:08 -0700

    Add additional basic server tests; Rename server testing files;

--- jest_tests/server_functions_working/user.js
+++
@@ -1,8 +0,0 @@
-// This module is supposed to fetch user data from an API
-// and return the user name
-// We expect this to return a promise
-import request from './request';
-
-export function getMacBookData() {
-  return request('/app/home').then(macBookProData => macBookProData);
-}
